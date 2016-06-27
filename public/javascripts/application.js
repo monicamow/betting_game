813d9a039cf86a0abf27e11b8c0e578543974174
Monica Mow <mow.monica@gmail.com>
2016-06-27 13:22:06 -0700

    Add first game logic - allow number guess
    - prompt user to guess number between 1 and 10
    - game should generate a random number between 1 and 10
    - validate player's guess against the game's number choice
    - alert the player that they guessed right or wrong with alerts and if/else statement

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,19 @@ window.onload = function() {
   var playerBankroll = 100;
   document.getElementById("bankroll").innerHTML = "Your initial bankroll is " + "$" + playerBankroll + ".";
 
-  var playerBet = prompt("Place a $5 or $10 bet.");
+  var playerBet = prompt("Place a $5 or $10 bet: ");
 
-  console.log(playerBet);
+  var playerGuess = prompt("Guess a number between 1 and 10: ");
+
+  var gameNumber = Math.floor((Math.random() * 10) + 1);
+
+  if (gameNumber === playerGuess) {
+    var validateGuess = "Good guess!";
+    alert(validateGuess);
+  }
+  else {
+    var validateGuess = "NOPE TRY AGAIN";
+    alert(validateGuess + "\nYou guessed " + playerGuess + ", but you should have guessed " + gameNumber + ".");
+  }
+
 }
