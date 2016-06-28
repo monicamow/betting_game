1803ea09670a884038426c8cee3e8e0378bf27fe
Monica Mow <mow.monica@gmail.com>
2016-06-27 19:44:57 -0700

    Add functions to capture form values
    - capture value of bet (radio buttons)
      > get input value by name and see if checked
    - capture value of guess
    - prevent form submission post method with preventDefault

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,59 +1,8 @@
-var app = function(event) {
-  
-  var playerBankroll = 100;
-  document.getElementById("bankroll").innerHTML = "Your initial bankroll is " + "$" + playerBankroll + ".";
-
-  while (playerBankroll > 0) {
-    var playerBet = prompt("Place a $5 or $10 bet: ");
-
-    var playerGuess = prompt("Guess a number between 1 and 10: ");
-
-    var gameNumber = Math.floor((Math.random() * 10) + 1);
-    console.log(playerGuess, gameNumber);
-
-    if (!playerBet || !gameNumber) {
-      break;
-    }
-
-    switch (validateGuess(playerGuess, gameNumber)) {
-      case 0:
-        alert("Good guess!");
-        playerBankroll += playerBet;
-        break;
-
-      case 1:
-        alert("Close but no cigar.");
-        break;
-
-      default:
-        alert("PLAY AGAIN");
-        playerBankroll -= playerBet;
-    }
-
-    document.getElementById("new-bankroll").innerHTML = "You now have " + "$" + playerBankroll + ".";
-  }
-  // if (gameNumber === playerGuess) {
-  //   var validateGuess = "Good guess!";
-  //   alert(validateGuess);
-  // }
-  // else {
-  //   var validateGuess = "NOPE TRY AGAIN";
-  //   alert(validateGuess + "\nYou guessed " + playerGuess + ", but you should have guessed " + gameNumber + ".");
-  //   if (gameNumber === playerGuess - 1 || gameNumber === playerGuess + 1) {
-  //     playerBankroll = playerBankroll;
-  //   }
-  //   else {
-  //     playerBankroll -= playerBet;
-  //   }
-  //   document.getElementById("new-bankroll").innerHTML = "You now have " + "$" + playerBankroll + ".";
-  // }
-
-}
-
-var validateGuess = function(playerGuess, gameNumber) {
-  return Math.abs(playerGuess - gameNumber);
-}
-
-
-
-document.addEventListener('DOMContentLoaded', app)
+$( document ).ready(function() {
+  $( "#player-input" ).submit(function( event ) {
+    event.preventDefault();
+    
+    $( "#bet" ).text( "Your bet is " + $('input[name="player-bet"]:checked').val() ).show();
+    $( "#guess" ).text( "Your guess is " + $("#player-guess").val() + "." ).show();
+  })
+});
