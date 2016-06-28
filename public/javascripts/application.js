04e34e1ed2d15ca8fee2d1aacd894d74a23eb08c
Monica Mow <mow.monica@gmail.com>
2016-06-28 00:35:28 -0700

    This game actually works now
    - finally made the document load before entering the while loop
      > required setting the initial bankroll to 0 and then invoking the app() function which contains the REPL and then putting the initial bankroll amount in the .ready() to $100
    - when bankroll = 0, flash 'game over' with red css
    - added reset form mechanism to each case in the switch statement

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,64 @@
-$( document ).ready(function() {
-  $( "#player-input" ).submit(function( event ) {
-    event.preventDefault();
+var app = function() {
+
+  do {
+
+    $( "#player-input" ).submit(function( event ) {
+      event.preventDefault();
+
+      playerBet = parseInt($('input[name="player-bet"]:checked').val());
+      playerGuess = parseInt($("#player-guess").val());
+
+      var gameNumber = Math.floor((Math.random() * 10) + 1);
+
+      $( "#bet" ).text( "Your bet is " + playerBet ).show();
+      $( "#guess" ).text( "Your guess is " + playerGuess + "." ).show();
+      $( "#game-number" ).text( "The actual number is " + gameNumber + "." ).show();      
     
-    $( "#bet" ).text( "Your bet is " + $('input[name="player-bet"]:checked').val() ).show();
-    $( "#guess" ).text( "Your guess is " + $("#player-guess").val() + "." ).show();
-  })
+
+      switch (validateGuess(playerGuess, gameNumber)) {
+        case 0:
+          $( "#result" ).text( "You are CORRECT!" ).show();
+          playerBankroll += playerBet;
+          $('#player-input').trigger("reset");
+          break;
+
+        case 1:
+          $( "#result" ).text( "Close but no cigar." ).show(); 
+          playerBankroll = playerBankroll;
+          $('#player-input').trigger("reset");
+          break;
+
+        default:
+          $( "#result" ).text( "Sorry, that was not even close..." ).show(); 
+          playerBankroll -= playerBet;
+          $('#player-input').trigger("reset");
+          break;
+        }
+      $( "#player-bankroll" ).text( "Bankroll: " + playerBankroll ).show(); 
+      if (playerBankroll == 0) {
+        $( "#game-over").text("GAME OVER").show();
+      }
+      })
+  } while (playerBankroll > 0);
+
+};
+
+var validateGuess = function(playerGuess, gameNumber) {
+  console.log(playerGuess);
+  return Math.abs(playerGuess - gameNumber);
+}
+
+var resetForm = function() {
+  $(':input','#player-input')
+  .not(':button, :submit, :reset, :hidden')
+  .val('')
+  .removeAttr('checked')
+  .removeAttr('selected');
+}
+
+$(document).ready(function() {
+  playerBankroll = 0;
+  console.log("The DOM is now loaded.");
+  app();
+  playerBankroll = 15;
 });
